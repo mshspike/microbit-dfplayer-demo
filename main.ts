7bd228fc1ac577c2b6bfb334b28cdbf2861942cf
Spike Mak <msh.spike@gmail.com>
2025-05-08 00:19:28 +0800

    Add "change volume" and "play/pause" mechanism

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
-input.onButtonPressed(Button.AB, function () {
-    dfplayermini.press(dfplayermini.playType.Pause)
+input.onButtonPressed(Button.A, function () {
+    volume += -1
+    dfplayermini.setVolume(volume)
 })
+input.onButtonPressed(Button.AB, function () {
+    if (status == 1) {
+        dfplayermini.press(dfplayermini.playType.Pause)
+        status = 0
+    } else {
+        dfplayermini.press(dfplayermini.playType.Play)
+        status = 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    volume += 1
+    dfplayermini.setVolume(volume)
+})
+let volume = 0
+let status = 0
+status = 1
+volume = 15
 dfplayermini.connect(SerialPin.P0, SerialPin.P1)
-dfplayermini.setVolume(15)
 dfplayermini.playFile(1, dfplayermini.isRepeat.No)
